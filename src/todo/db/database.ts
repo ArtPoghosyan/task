562d6453b90f462930eb5fb6f9fe3cac0fe2d81c
ArtPoghosyan <arturp@mfmsoft.com>
2025-12-23 18:34:07 +0400

    first commit

--- src/todo/db/database.ts
+++ src/todo/db/database.ts
@@ -1,17 +1,32 @@
-import fs from 'node:fs/promises';
 import path from 'node:path';
+import fs from 'node:fs';
 import { TODO, UPDATED_TODO } from '../../typescript/todo/todo.interfaces';
 import todoDB from './todo.json';
 const db: { [key: string]: TODO } = todoDB;
 
 export class DataBase {
   static async save(): Promise<boolean> {
-    await fs.writeFile(
-      path.resolve(__dirname.replace('dist', 'src'), 'todo.json'),
-      JSON.stringify(db, null, 2),
-    );
-    return true;
+    return new Promise((resolve, reject) => {
+      const filePath = path.resolve(
+        __dirname.replace('dist', 'src'),
+        'todo.json',
+      );
+
+      const writeStream = fs.createWriteStream(filePath, { encoding: 'utf-8' });
+
+      writeStream.write(JSON.stringify(db, null, 2));
+      writeStream.end();
+
+      writeStream.on('finish', () => {
+        resolve(true);
+      });
+
+      writeStream.on('error', (err) => {
+        reject(err);
+      });
+    });
   }
+
   static async create(obj: TODO): Promise<TODO | false> {
     try {
       db[obj.id] = obj;
